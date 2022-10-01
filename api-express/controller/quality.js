2497df586fb6ad5aa220bb0c0aad0bf4236cc2c4
kannangr21 <kannanravi2105@gmail.com>
2022-10-01 11:29:59 +0530

    Quality model change

--- api-express/controller/quality.js
+++ api-express/controller/quality.js
@@ -16,7 +16,8 @@ router.post('/putData', (req, res) => {
         tss: req.body.tss,
         pH: req.body.pH,
         turbidity: req.body.turbidity,
-        temperature: req.body.temperature,
+        waterTemperature: req.body.waterTemperature,
+        atmosphericTemperature: req.body.atmosphericTemperature,
         disOxygen: req.body.disOxygen
     }).then((quality) => {
         return res.status(201).json({success: true, message: "Data Added Successfully"});    
@@ -28,6 +29,15 @@ router.post('/putData', (req, res) => {
 router.get('/getAllData', (req, res) => {
     Quality.find({}).then((qual) => {
         return res.status(200).json({success: true, quality: qual});
+    }).catch((err) => {
+        return res.status(500).json({success: false, message: err.message});
+    });
+});
+
+router.get('/getByNode/:nodeId', (req, res) => {
+    Quality.find({nodeId: req.params.nodeId})
+    .then((values) => {
+        return res.status(200).json({success: true, values: values});
     }).catch((err) => {
         return res.status(500).json({success: false, message: err.message});
     });
